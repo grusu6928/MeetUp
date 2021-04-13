cab488848c4ce95539a9961407e835248c01b700
george <grus6928@gmail.com>
2021-04-12 17:57:47 -0700

    CORS debugging

--- src/main/java/edu/brown/cs/student/stars/Main.java
+++ src/main/java/edu/brown/cs/student/stars/Main.java
@@ -81,6 +81,22 @@ public final class Main {
   
     DataBase.connect();
     Spark.port(port);
+    Spark.options("/*", (request, response) -> {
+      String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+      if (accessControlRequestHeaders != null) {
+        response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+      }
+
+      String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+
+      if (accessControlRequestMethod != null) {
+        response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+      }
+
+      return "OK";
+});
+
+Spark.before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
     Spark.externalStaticFileLocation("src/main/resources/static");
     Spark.exception(Exception.class, new ExceptionPrinter());
     FreeMarkerEngine freeMarker = createEngine();
