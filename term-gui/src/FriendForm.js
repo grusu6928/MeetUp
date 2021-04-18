4fccbf67097062046c2fd15f82524ee36eede393
george <grus6928@gmail.com>
2021-04-17 20:47:09 -0700

    added friendsList routing logic - need to figure out response data object

--- term-gui/src/FriendForm.js
+++ term-gui/src/FriendForm.js
@@ -2,39 +2,13 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 
-function FriendForm( {addFriend}) {
+function FriendForm( {addFriend, request}) {
     const [value, setValue] = useState("");
-
-    const requestFriends = (reqType, newFriend, removeFriend) => {
-      // 0 = query friends 
-      // 1 = add friend 
-      // 2 = kill friend
-      const toSend = {
-          requestType = reqType,
-          userID: localStorage.getItem("user"),
-          userToAdd: newFriend,
-          userToRemove: removeFriend
-      }
-      let config = {
-          headers: {
-            "Content-Type": "application/json",
-            'Access-Control-Allow-Origin': '*',
-            }
-          }
-          axios.post('http://localhost:4567/friends', toSend, config)
-          .then(response => {
-              // want to update event information
-              //setFL()
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-        }
-      
 
     const handleSubmit = e => {
       e.preventDefault();
       if (!value) return;
+      request("insert", value, "")
       addFriend(value);
       setValue("");
     };
